56de39df613bae84687fbc4233c18dec23b1515f
slavb <slavb@ilb.ru>
2019-12-23 04:41:13 +0000

    async config init

--- filedossier-js/conf/config.js
+++ filedossier-js/conf/config.js
@@ -1,14 +1,16 @@
 import { DossiersApi, ApiClient as DossierApiClient } from '@ilb/filedossier-api';
 import { ApiClient } from '@ilb/js-auto-proxy';
-require('@ilb/node_context').config({ debug: true });
 
 const config = {};
-if (!process.browser) {
-  config.certfile = process.env['ru.bystrobank.apps.workflow.certfile'];
-  config.passphrase = process.env['ru.bystrobank.apps.workflow.cert_PASSWORD'];
-  const fs = require('fs');
-  config.cert = config.certfile !== null ? fs.readFileSync(config.certfile) : null;
-  config.ca = process.env.NODE_EXTRA_CA_CERTS ? fs.readFileSync(process.env.NODE_EXTRA_CA_CERTS) : null;
+function fillparams() {
+  if(!config.paramsloaded) {
+    config.certfile = process.env['ru.bystrobank.apps.workflow.certfile'];
+    config.passphrase = process.env['ru.bystrobank.apps.workflow.cert_PASSWORD'];
+    const fs = require('fs');
+    config.cert = config.certfile !== null ? fs.readFileSync(config.certfile) : null;
+    config.ca = process.env.NODE_EXTRA_CA_CERTS ? fs.readFileSync(process.env.NODE_EXTRA_CA_CERTS) : null;
+    config.paramsloaded = true;
+  }
 }
 
 /**
@@ -18,6 +20,7 @@ if (!process.browser) {
 */
 const applyAuthToRequest = ({ request, xRemoteUser }) => {
   if (!process.browser) {
+    fillparams();
     request
       .ca(config.ca)
       .key(config.cert)
