e46f7d05d15a8951ea602126d6fcf5c5ff6ed5c3
slavb <slavb@ilb.ru>
2020-03-11 06:20:15 +0000

    offile semantic fonts

--- filedossier-js/pages/dossier/table.js
+++ filedossier-js/pages/dossier/table.js
@@ -1,4 +1,4 @@
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-offline/semantic.min.css';
 import PropTypes from 'prop-types';
 import FileDossier from '../../classes/FileDossier';
 import Dossier from '../../components/Dossier';
