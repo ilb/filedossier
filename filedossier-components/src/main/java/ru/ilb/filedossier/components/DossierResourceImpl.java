4a7e8dbf122abe4b0f1655fdb0232692c1a6ac74
Vjacheslav Borisov <slavb@ilb.ru>
2020-07-06 15:10:54 +0400

    dossierStoreResource

--- filedossier-components/src/main/java/ru/ilb/filedossier/components/DossierResourceImpl.java
+++ filedossier-components/src/main/java/ru/ilb/filedossier/components/DossierResourceImpl.java
@@ -78,4 +78,9 @@ public class DossierResourceImpl implements DossierResource {
         applicationContext.getAutowireCapableBeanFactory().autowireBean(resource);
         return resourceContext.initResource(resource);
     }
+
+    @Override
+    public DossierStoreResource getDossierStoreResource() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
