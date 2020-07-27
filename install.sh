8c2d834d4183dd99af87b0e96762e44426aaadd4
Vjacheslav Borisov <slavb@ilb.ru>
2020-07-27 10:53:38 +0400

    add install files

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ set -e
 mkdir -p /home/filedossier/packages
 mkdir -p /var/apps/filedossier/files
 cp -r filedossier-web/src/main/resources/models/* /home/filedossier/packages
+cp -r filedossier-web/src/main/resources/teststoreroot /var/apps/filedossier/files
