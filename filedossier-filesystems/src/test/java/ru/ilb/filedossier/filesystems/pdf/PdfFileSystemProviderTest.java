fdf53e90b6f1c159cc67b7c304411b9ddf4e69a8
Maxim Kuznetsov <kuznetsov_me@bystrobank.ru>
2020-12-01 13:36:18 +0400

    style: Remove unused imports

--- filedossier-filesystems/src/test/java/ru/ilb/filedossier/filesystems/pdf/PdfFileSystemProviderTest.java
+++ filedossier-filesystems/src/test/java/ru/ilb/filedossier/filesystems/pdf/PdfFileSystemProviderTest.java
@@ -25,8 +25,8 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
